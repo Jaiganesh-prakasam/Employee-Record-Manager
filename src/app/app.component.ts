edc1a1841211d31f30522dcd191d0b6e03afcf3b
Jai ganesh Prakasam <jaiganesh.prakasam@gmail.com>
2025-03-15 14:44:07 +0530

    app component turned into a standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [RouterModule],
+    standalone: true
 })
 export class AppComponent {
   title = 'Employee-Record-Manager';
